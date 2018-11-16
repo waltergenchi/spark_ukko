96f6a37c5660e0cf6a2a80c26ade8b1eb6d3b0d1
waltergenchi <walter.genchi@gmail.com>
2018-11-16 12:04:16 +0200

    add script

---
+++ script_nmohan.py
@@ -0,0 +1,22 @@
+from pyspark import SparkContext, SparkConf
+import os
+import sys
+
+dataset = "/wrk/nmohan/distsys-dir/datasets/data-1-sample.txt" ##change this to your dataset path
+
+conf = (SparkConf()
+        .setAppName("nmohan")           ##change app name to your username
+        .setMaster("spark://128.214.48.227:7077")
+        .set("spark.cores.max", "10")  ##dont be too greedy ;)
+        .set("spark.rdd.compress", "true")
+        .set("spark.broadcast.compress", "true"))
+sc = SparkContext(conf=conf)
+
+data = sc.textFile(dataset)
+data = data.map(lambda s: float(s))
+
+count = data.count()
+sum = data.sum()
+
+print "Count = %.8f" % count
+print "Sum = %.8f" % sum
