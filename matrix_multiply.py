d4b0e97a972ec206c6cb9ca677f8556075051532
waltergenchi <walter.genchi@gmail.com>
2018-11-23 16:06:51 +0200

    a

--- matrix_multiply.py
+++ matrix_multiply.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np
 
 def multiply(row):
-    return np.outer(row,row).reshape(1,4)
+    return np.outer(row,row).reshape(1,nCol)
     '''
     multiply_row = []
     for i in row:
