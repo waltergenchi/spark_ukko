2d4c88831594a6212417e575d375e41459894f22
waltergenchi <walter.genchi@gmail.com>
2018-11-24 17:50:20 +0200

    a

--- matrix_multiply.py
+++ matrix_multiply.py
@@ -55,7 +55,7 @@ def main():
 
 # doing the sum coloumn by coloumn
     print("**************\n\n\n\n\n\n\n Mapping Operation \n\n\n\n\n\n\n\n\n***********")
-    row_permutation = matrix.mapPartitions(lambda row: multiply(row))
+    row_permutation = matrix.map(lambda row: multiply(row))
     #print(row_permutation.collect())
     print("**************\n\n\n\n\n\n\n Reduce Operation \n\n\n\n\n\n\n\n\n***********")
     row_permutation = row_permutation.reduce(add)
