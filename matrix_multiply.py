73ce1d716fb157b41e2095f921fd47856d1a9b13
waltergenchi <walter.genchi@gmail.com>
2018-11-24 16:55:13 +0200

    a

--- matrix_multiply.py
+++ matrix_multiply.py
@@ -42,7 +42,7 @@ def main():
 
     raw_matrix_file = sc.textFile(dataset)
     # Read matrix from file and split the lines based on space and use float for items
-    matrix = raw_matrix_file.map(lambda line: line.split()).map(lambda value: [float(i) for i in value])
+    matrix = raw_matrix_file.map(lambda line: line.split()).map(lambda value: [float(i) for i in value]).cache()
     #print(matrix.collect())
     print("read file")
 
