6c2031fd6f1373497ed7997880f9b8c53eaec520
waltergenchi <walter.genchi@gmail.com>
2018-11-27 21:19:04 +0200

    a

--- spark2_first_sol.py
+++ spark2_first_sol.py
@@ -12,7 +12,7 @@ def multiply(row):
 
 def main():
     #dataset = "data-2-105.txt" # dataset with 10^5 rows, works fine in almost 70 seconds
-    dataset = "stupido.txt" # dataset with 10^3 rows, work fine in almost 5 seconds
+    dataset = "data-2-sample.txt" # dataset with 10^3 rows, work fine in almost 5 seconds
 
     conf = (SparkConf()
             .setAppName("genchi")           ##change app name to your username
@@ -90,7 +90,8 @@ def main():
     print("FINAL NUMBER OF COLUMNS : %d" %nCol[0])
 
     print("The type of A * A_transpose * A is %s\n\n" %type(Atranspose_A))
-    print(A_Atranspose_A.collect())
+    
+    #print(A_Atranspose_A.collect())
 
     # trying to save file as:
     # A_Atranspose_A=A_Atranspose_A.map(lambda line: str(row[1]))
