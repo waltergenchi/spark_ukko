db0927f2f2582ee056c38f19c36e9aad57c76fab
waltergenchi <walter.genchi@gmail.com>
2018-11-27 17:18:45 +0200

    a

--- spark2.py
+++ spark2.py
@@ -11,8 +11,8 @@ def multiply(row):
     return np.outer(row,row)
 
 def main():
-    dataset = "data-2-105.txt" # dataset with 10^5 rows, works fine in almost 70 seconds
-    #dataset = "data-2-sample.txt" # dataset with 10^3 rows, work fine in almost 5 seconds
+    #dataset = "data-2-105.txt" # dataset with 10^5 rows, works fine in almost 70 seconds
+    dataset = "data-2.txt" # dataset with 10^3 rows, work fine in almost 5 seconds
 
     conf = (SparkConf()
             .setAppName("genchi")           ##change app name to your username
